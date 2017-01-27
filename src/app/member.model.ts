8e468cf7792e0304541994b37258651028a0aa47
Jacob Hixon <jacobhixon47@gmail.com>
2017-01-27 11:48:04 -0800

    display members from firebase on members page

--- src/app/member.model.ts
+++ src/app/member.model.ts
@@ -1,2 +1,3 @@
 export class Member {
+  constructor (public name: string, public bio: string, public age: number, public rank: number) { }
 }
--- src/app/members-list/members-list.component.ts
+++ src/app/members-list/members-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-members-list',
   templateUrl: './members-list.component.html',
-  styleUrls: ['./members-list.component.scss']
+  styleUrls: ['./members-list.component.scss'],
+  providers: [MemberService]
 })
 export class MembersListComponent implements OnInit {
   members: FirebaseListObservable<any[]>;
-  constructor(private router: Router, public af: AngularFire) { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
